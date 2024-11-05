5227624b138fb6384c3297ab8f57c183db57dab3
alikazmi181293 <syed.ali.hussnain.kazmi@gmail.com>
2024-11-05 20:04:26 +0200

    Created database context for registration service. Added connection string the appsetting.json. Registered database context in the program.cs. Added necessary dependencies for entityframework.

---
+++ RegistrationService/Persistence/RegistrationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RegistrationService.Models;
+
+namespace RegistrationService.Persistence
+{
+    public class RegistrationDbContext : DbContext
+    {
+        DbSet<Registration> Registrations { get; set; }
+
+        public RegistrationDbContext(DbContextOptions<RegistrationDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
