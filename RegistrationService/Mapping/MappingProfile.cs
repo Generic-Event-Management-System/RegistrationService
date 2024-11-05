fe849382919e29bf0759f2cf38fde4d7f4a13c87
alikazmi181293 <syed.ali.hussnain.kazmi@gmail.com>
2024-11-05 20:40:43 +0200

    Added auto mapper in the project. Created mapping profile class to map the registration entity and dto objects.

---
+++ RegistrationService/Mapping/MappingProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using RegistrationService.Constants;
+using RegistrationService.Models.Dto;
+using RegistrationService.Models.Entities;
+
+namespace RegistrationService.Mapping
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<RegistrationDto, Registration>()
+                .ForMember(dest => dest.Status, opt => opt.MapFrom((src, dest) => string.IsNullOrEmpty(dest.Status) ? RegistrationStatus.Pending : dest.Status))
+                .ForMember(dest => dest.Date, opt => opt.MapFrom((src, dest) => string.IsNullOrEmpty(dest.Date) ? DateTime.UtcNow.ToString("yyyy-MM-dd") : dest.Date));
+
+            CreateMap<Registration, RegistrationDto>();
+        }
+    }
+}
