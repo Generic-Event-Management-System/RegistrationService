507d027123729767dab34e79230479197c3431a6
alikazmi181293 <syed.ali.hussnain.kazmi@gmail.com>
2024-11-05 20:38:54 +0200

    Created a static registration status class to streamline status across application and avoid magic strings.

---
+++ RegistrationService/Constants/RegistrationStatus.cs
@@ -0,0 +1,9 @@
+﻿namespace RegistrationService.Constants
+{
+    public static class RegistrationStatus
+    {
+        public static string Pending = "Pending";
+        public static string Confirmed = "Confirmed";
+        public static string Cancelled = "Cancelled";
+    }
+}
