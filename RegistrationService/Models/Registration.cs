a728f3c9adee0cb84bb9077ab79156bb48ea1da5
alikazmi181293 <syed.ali.hussnain.kazmi@gmail.com>
2024-11-05 20:02:31 +0200

    Added Registration entity in the project

---
+++ RegistrationService/Models/Registration.cs
@@ -0,0 +1,11 @@
+﻿namespace RegistrationService.Models
+{
+    public class Registration
+    {
+        public int Id { get; set; }
+        public required int EventId { get; set; }
+        public required int TeamId { get; set; }
+        public required string Status { get; set; }
+        public required string Date { get; set; }
+    }
+}
